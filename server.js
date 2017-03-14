3d16e0665c5003b06bb4a9a337c322004fd34e00
noel paganelli <noel.paganelli@free.fr>
2017-03-14 10:32:44 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
